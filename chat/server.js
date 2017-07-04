3639a40dc0615823717deb8cbcaabab92d358a4f
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-07-04 17:04:46 -0500

    Finish basic chat with Socket.io

--- chat/server.js
+++ chat/server.js
@@ -9,6 +9,8 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
+let users = [];
+
 // View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -16,9 +18,38 @@ app.set('view engine', 'pug');
 // Static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Socket.io connect
+io.sockets.on('connection', socket => {
+  // set username
+  socket.on('set user', (data, callback) => {
+    if (users.indexOf(data) != -1) {
+      callback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsers();
+    }
+  });
+
+  socket.on('send message', data => {
+    io.sockets.emit('show message', { msg: data, user: socket.username });
+  });
+
+  socket.on('disconnect', data => {
+    if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsers();
+  });
+
+  function updateUsers() {
+    io.sockets.emit('users', users);
+  }
+});
+
 // Index route
 app.get('/', (req, res, next) => {
-  res.send('Hello World');
+  res.render('index');
 });
 
 server.listen(port, () => {
