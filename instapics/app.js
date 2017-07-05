1033fc94b853f5b165421c7d15c8364f508eaa45
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-07-04 21:20:00 -0500

    Create instapics

--- instapics/app.js
+++ instapics/app.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const api = require('instagram-node').instagram();
 
 var app = express();
 
@@ -21,19 +20,114 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+  })
+);
 
-app.use('/', index);
-app.use('/users', users);
+// Gloabl Vars
+app.use((req, res, next) => {
+  // format date
+  res.locals.formatDate = function(date) {
+    let myDate = new Date(date * 1000);
+    return myDate.toLocaleString();
+  };
+
+  // Is user logged in?
+  if (req.session.accesstoken && req.session.accesstoken != 'undefined') {
+    res.locals.isLoggedIn = true;
+  } else {
+    res.locals.isLoggedIn = false;
+  }
+  next();
+});
+
+// Instagram stuff
+api.use({
+  client_id: '72924f571ac64d57ad2e6f7b45fcf202',
+  client_secret: '7b86c5af3d41452c8ae9b6a3311188c8'
+});
+
+const redirect_uri = 'http://localhost:3000/handleauth';
+
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+};
+
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, (err, result) => {
+    if (err) {
+      console.log(err.body);
+      res.send("Didn't work");
+    } else {
+      console.log(`Access Token: ${result.access_token}`);
+      console.log(`User ID: ${result.user.id}`);
+      //res.send('You made it!!');
+
+      req.session.accesstoken = result.access_token;
+      req.session.uid = result.user.id;
+
+      api.use({ access_token: req.session.accesstoken });
+
+      res.redirect('/main');
+    }
+  });
+};
+
+// Login Route
+app.get('/login', exports.authorize_user);
+
+// Handleauth
+app.get('/handleauth', exports.handleauth);
+
+// Index Route
+app.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Welcome'
+  });
+});
+
+// Main Route
+app.get('/main', (req, res, next) => {
+  api.user(req.session.uid, (err, result, remaining, limit) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      api.user_media_recent(req.session.uid, {}, (err, medias, pagination, remaining, limit) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.render('main', {
+            title: 'My Instagram',
+            user: result,
+            medias: medias
+          });
+        }
+      });
+    }
+  });
+});
+
+// Logout Route
+app.get('/logout', (req, res, next) => {
+  req.session.accesstoken = false;
+  req.session.uid = false;
+  res.redirect('/');
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
