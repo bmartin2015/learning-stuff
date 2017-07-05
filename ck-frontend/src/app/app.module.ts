b14ab9492f469afb91f25d0c6f36f233c16675b4
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-07-04 19:38:20 -0500

    Add angular build files to express

--- ck-frontend/src/app/app.module.ts
+++ ck-frontend/src/app/app.module.ts
@@ -4,17 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { ClientService } from './services/client.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
+  declarations: [AppComponent, ClientsComponent, NavbarComponent],
+  imports: [BrowserModule, FormsModule, HttpModule],
+  providers: [ClientService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
